a30beb51b56d9290ae37ccf273af7eb30851a2bf
Zerozany <691139128@qq.com>
2025-12-04 15:11:59 +0800

    update:删除了主题配置文件代码

--- src/ThemeManager/ThemeManager.cpp
+++ src/ThemeManager/ThemeManager.cpp
@@ -1,24 +1,7 @@
 #include "ThemeManager.h"
 #include <QJSEngine>
 #include <QQmlEngine>
-#include <QSettings>
-#include <QStandardPaths>
 #include <QDir>
-
-Q_INVOKABLE QString ThemeManager::currentThemeName() const
-{
-    return m_currentThemeName;
-}
-
-Q_INVOKABLE void ThemeManager::setCurrentThemeName(const QString& _currentThemeName)
-{
-    if (m_currentThemeName == _currentThemeName)
-    {
-        return;
-    }
-    m_currentThemeName = _currentThemeName;
-    Q_EMIT this->currentThemeNameChanged();
-}
 
 QVariantMap ThemeManager::currentTheme() const
 {
@@ -79,28 +62,11 @@ ThemeManager::ThemeManager(QObject* _parent) : QObject{_parent}
 
 auto ThemeManager::init() noexcept -> void
 {
-    m_settings = new QSettings{QDir{QStandardPaths::writableLocation(QStandardPaths::AppDataLocation)}.filePath("config/settings.ini"), QSettings::IniFormat, this};
-    // qDebug() << m_settings->fileName();
-    m_settings->beginGroup("Themes");
-    if ((m_settings->contains("ThemeName") && !m_settings->value("ThemeName").isNull()) || !m_settings->contains("ThemeName"))
-    {
-        m_settings->setValue("ThemeName", "lightTheme");
-        this->setCurrentThemeName("lightTheme");
-    }
-    m_settings->endGroup();
+    this->setCurrentTheme(Themes::lightTheme);
     this->setFontSize(Themes::fontSize);
     this->setStyleSize(Themes::styleSize);
 }
 
 auto ThemeManager::connectSignal2Slot() noexcept -> void
 {
-    connect(this, &ThemeManager::currentThemeNameChanged, this, &ThemeManager::onCurrentThemeNameChanged);
-}
-
-void ThemeManager::onCurrentThemeNameChanged()
-{
-    if (m_currentThemeName == "lightTheme")
-    {
-        this->setCurrentTheme(Themes::lightTheme);
-    }
 }
--- src/ThemeManager/ThemeManager.h
+++ src/ThemeManager/ThemeManager.h
@@ -16,23 +16,18 @@ _Pragma("once");
 
 class QJSEngine;
 class QQmlEngine;
-class QSettings;
 
 class QZERO_API ThemeManager : public QObject
 {
     Q_OBJECT
     QML_SINGLETON
     QML_ELEMENT
-    Q_PROPERTY(QString currentThemeName READ currentThemeName WRITE setCurrentThemeName NOTIFY currentThemeNameChanged);
     Q_PROPERTY(QVariantMap currentTheme READ currentTheme WRITE setCurrentTheme NOTIFY currentThemeChanged);
     Q_PROPERTY(QVariantMap fontSize READ fontSize WRITE setFontSize NOTIFY fontSizeChanged);
     Q_PROPERTY(QVariantMap styleSize READ styleSize WRITE setStyleSize NOTIFY styleSizeChanged);
 
 public:
     ~ThemeManager() noexcept = default;
-
-    Q_INVOKABLE QString currentThemeName() const;
-    Q_INVOKABLE void    setCurrentThemeName(const QString& _currentThemeName);
 
     Q_INVOKABLE QVariantMap currentTheme() const;
     Q_INVOKABLE void        setCurrentTheme(const QVariantMap& _currentTheme);
@@ -54,7 +49,6 @@ private:
     auto connectSignal2Slot() noexcept -> void;
 
 Q_SIGNALS:
-    void currentThemeNameChanged();
 
     void currentThemeChanged();
 
@@ -63,11 +57,8 @@ Q_SIGNALS:
     void styleSizeChanged();
 
 private Q_SLOTS:
-    void onCurrentThemeNameChanged();
 
 private:
-    QSettings*  m_settings{nullptr};
-    QString     m_currentThemeName{};
     QVariantMap m_currentTheme{};
     QVariantMap m_fontSize{};
     QVariantMap m_styleSize{};
