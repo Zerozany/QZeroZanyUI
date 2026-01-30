5c972ef73c9bb5c514693e38ae2b0426bb35fcfd
Zerozany <691139128@qq.com>
2026-01-30 15:00:35 +0800

    refactor:修改了动态插件dll的生成路径

--- cmake/importQConf.cmake
+++ cmake/importQConf.cmake
@@ -10,3 +10,9 @@ set_target_properties(${PROJECT_NAME} PROPERTIES
     MACOSX_BUNDLE "$<$<CONFIG:Release>:TRUE>$<$<NOT:$<CONFIG:Release>>:FALSE>"
     WIN32_EXECUTABLE "$<$<CONFIG:Debug>:FALSE>$<$<NOT:$<CONFIG:Debug>>:TRUE>"
 )
+
+add_custom_command(TARGET "${PROJECT_NAME}plugin" POST_BUILD
+    COMMAND ${CMAKE_COMMAND} -E copy
+    $<TARGET_FILE:${PROJECT_NAME}plugin>
+    "${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}plugin.dll"
+)
